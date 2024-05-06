f480a94dca434acd649a2dfa5cdeb9f090dddcf7
Lyshka <53864647+Lyshka@users.noreply.github.com>
2024-05-06 20:00:49 +0300

    add page

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
-import { defineConfig } from 'vite';
-import injectHTML from 'vite-plugin-html-inject';
+import { defineConfig } from "vite";
+import injectHTML from "vite-plugin-html-inject";
+import { resolve } from "path";
 
 export default defineConfig({
-	plugins: [injectHTML()],
+  plugins: [injectHTML()],
+  build: {
+    rollupOptions: {
+      input: {
+        visual: resolve(__dirname, "visual.html"),
+        raspil: resolve(__dirname, "index.html"),
+      },
+    },
+  },
 });
